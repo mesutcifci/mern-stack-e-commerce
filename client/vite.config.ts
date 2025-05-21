6a824461af59885e6fe8df73a7dc9ae6ad6442d2
mesutcifci <cfcmesut@gmail.com>
2025-05-21 22:53:27 +0300

    refactor(client): update type aliases in configuration files

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     alias: {
       "@assets": path.resolve(__dirname, "src/assets"),
       "@components": path.resolve(__dirname, "src/components"),
+      "@types": path.resolve(__dirname, "src/types"),
     },
   },
   test: {
