2aeab60394c4c889ddc65c8262dfb44f0b7b742b
mesutcifci <cfcmesut@gmail.com>
2025-05-23 22:44:54 +0300

    refactor(client): use switch case for mapping components for readability

--- client/src/utils/componentMapper.tsx
+++ client/src/utils/componentMapper.tsx
@@ -1,29 +1,12 @@
-import React from "react";
 import { IComponent } from "@custom-types/components";
 import { HeroSlider } from "@components/HeroSlider";
-import { ISliderComponent } from "@custom-types/components/slider-component";
 
-const componentMap: Record<
-  string,
-  (props: { data: IComponent }) => JSX.Element
-> = {
-  "slider-component": ({ data }) => (
-    <HeroSlider data={data as ISliderComponent} />
-  ),
-};
-
-export const ComponentRenderer = ({
-  data,
-}: {
-  data: IComponent;
-  index?: number;
-}): JSX.Element | null => {
-  const ComponentToRender = componentMap[data.type];
-
-  if (!ComponentToRender) {
-    console.warn(`No component found for type: ${data.type}`);
-    return null;
+// TODO: use a readable map approach for components, instead of switch case
+export const ComponentRenderer = ({ data }: { data: IComponent }) => {
+  switch (data.type) {
+    case "slider-component":
+      return <HeroSlider data={data} />;
+    default:
+      return null;
   }
-
-  return <ComponentToRender data={data} />;
 };
