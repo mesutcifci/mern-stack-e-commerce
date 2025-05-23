9ae963f94b9eabd45ea76a1b970c827ddc0504c2
mesutcifci <cfcmesut@gmail.com>
2025-05-23 23:27:31 +0300

    chore(client): update .gitignore and rename type alias in vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     alias: {
       "@assets": path.resolve(__dirname, "src/assets"),
       "@components": path.resolve(__dirname, "src/components"),
-      "@types": path.resolve(__dirname, "src/types"),
+      "@custom-types": path.resolve(__dirname, "src/types"),
     },
   },
   test: {
